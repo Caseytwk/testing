84f39841719c5244acf34e9fe6bee1351faec4d3
Caseytwk <casey.twk.02@gmail.com>
2025-04-08 21:38:46 +0800

    Add files via upload

---
+++ v4/inference.py
@@ -0,0 +1,76 @@
+import joblib
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+import matplotlib.pyplot as plt
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+# Anomaly Data
+anomaly_data = {
+    "network_packet_size": 406.0,
+    "login_attempts": 9.0,
+    "session_duration": 191.052837,
+    "ip_reputation_score": 0.339711,
+    "failed_logins": 1.0,
+    "unusual_time_access": 0.0,
+    "protocol_type": "UDP",
+    "encryption_used": "DES",
+    "browser_type": "Edge",
+    "user_roles": "Employee"
+}
+
+# Categorical Features and Numerical Features
+cat_features = ["protocol_type", "encryption_used", "browser_type", "user_roles"]
+num_features = ["network_packet_size", "login_attempts", "session_duration", "ip_reputation_score", "failed_logins", "unusual_time_access"]
+
+def load_configurations():
+    # Load Configurations
+    scaler = joblib.load("../../model/v4/scaler.pkl") # MinMaxScaler
+    encoder = joblib.load("../../model/v4/encoder.pkl") # OneHotEncoder
+    threshold = joblib.load("../../model/v4/threshold.pkl") # 95th percentile
+    model = tf.keras.models.load_model("../../model/v4/autoencoder.h5") # Load Model
+    return scaler, encoder, threshold, model
+
+def preprocess_data(data, scaler, encoder):
+    # Convert to DataFrame
+    new_df = pd.DataFrame([data])
+
+    # Normalize numerical features
+    new_df[num_features] = scaler.transform(new_df[num_features])  # Use the same scaler!
+
+    # One-hot encode categorical features
+    new_encoded = encoder.transform(new_df[cat_features])
+    new_encoded_df = pd.DataFrame(new_encoded, columns=encoder.get_feature_names_out(cat_features))
+
+    # Merge preprocessed features
+    new_df = new_df.drop(columns=cat_features).reset_index(drop=True)
+    new_df = pd.concat([new_df, new_encoded_df], axis=1)
+
+    # Convert to NumPy array for TensorFlow
+    new_input = new_df.to_numpy()
+    return new_input
+
+
+def inferencing(new_input, model):
+    # Pass the input through the autoencoder
+    reconstructed = model.predict(new_input)
+
+    # Compute Reconstruction Error (MAE)
+    error = np.mean(np.abs(new_input - reconstructed))
+    return error
+
+scaler, encoder, threshold, model = load_configurations()
+new_input = preprocess_data(anomaly_data, scaler, encoder)
+error = inferencing(new_input, model)
+
+# Print the Reconstruction Error
+# print(f"Reconstruction Error: {error:.6f}")
+
+if error > threshold:
+    print("🚨 Anomalous Data Point Detected!")
+else:
+    print("✅ Normal Data Point")
